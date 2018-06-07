c6529555f7a450e2993099a13eff97d5a9130d2b
Liuhezhe <35425295+Liuhezhe@users.noreply.github.com>
2018-06-08 01:36:40 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', AIzaSyDAqepcU2MhdDftUmgNCtZomgC7bgOroKg );
+define('GJ_CODE', HK);
+define('SITE_NAME',Youtube);
+define('TITLENAME',Youtube);
+define('EN2DEKEY',ZFSJ1301220116*);
+define('EMAIL', hezheliu@gmail.com);
 ?>
